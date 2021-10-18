059509a03ba64bd358c15d2b17e4975c4173c25e
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-10-18 10:17:59 +0530

    Update loadbal.tf

--- loadbal.tf
+++ loadbal.tf
@@ -22,10 +22,10 @@ module "elb_http" {
     },
     {
       instance_port     = 443
-      instance_protocol = "http"
+      instance_protocol = "https"
       lb_port           = 443
-      lb_protocol       = "http"
-      ssl_certificate_id = "arn:aws:acm:eu-west-1:235367859451:certificate/6c270328-2cd5-4b2d-8dfd-ae8d0004ad31"
+      lb_protocol       = "https"
+      ssl_certificate_id = "arn:aws:acm:us-east-1:235367859451:certificate/6c270328-2cd5-4b2d-8dfd-ae8d0004ad31"
     },
   ]
 
