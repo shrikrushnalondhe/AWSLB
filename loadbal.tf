3f36544a00edf9eb239975a18a568b3243186f0c
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-10-18 10:44:48 +0530

    Update loadbal.tf

--- loadbal.tf
+++ loadbal.tf
@@ -26,7 +26,6 @@ module "elb" {
       instance_protocol = "https"
       lb_port           = 443
       lb_protocol       = "https"
-      ssl_certificate_id = "arn:aws:acm:us-east-1:235367859451:certificate/6c270328-2cd5-4b2d-8dfd-ae8d0004ad31"
     },
   ]
 
@@ -47,7 +46,7 @@ module "elb" {
   instances           = ["i-021ce51c8e99b3bef", "i-0d3795327779c4677"]
 
   tags = {
-    Owner       = "user"
-    Environment = "dev"
+   Environment1 = "dev"
+   Environment2 = "prod"
   }
 }
